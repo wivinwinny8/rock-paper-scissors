d6a4c06f2e48ed28ba314a8afeb6ff043e872d73
wivinwinny8 <wivinwinnyv@gmail.com>
2024-07-16 22:25:20 +0530

    -m

--- script.js
+++ script.js
@@ -6,35 +6,64 @@ let index;
 let cpuip;
 let result;
 function begin(){
+    document.getElementById("btnbox").classList.add("fadeIn");
     document.getElementById("btnbox").style.visibility="visible";
-    document.getElementById("start").style.visibility="hidden";
-    document.getElementById("score").style.visibility="visible";
+    
+
+    document.getElementById("start").classList.add("fadeOut");
+    setTimeout(function(){
+        document.getElementById("start").style.visibility="hidden";
+    },501);
+    
+    document.getElementById("score").classList.add("fadeIn");
+    setTimeout(
+        function(){
+            document.getElementById("score").style.visibility="visible";
+        },10
+    )
+    
+
     document.getElementById("retry").style.visibility="hidden";
-    document.getElementById("result").textContent="";
-    document.getElementById("cpuimg").src="/asset/papercpu.png";
-    document.getElementById("userimg").src="/asset/paperplayer.png";
+
+    document.getElementById("out").textContent="Achieve 5 points to claim the win.";
+    document.getElementById("cpuimg").src="/asset/rockcpu.png";
+    document.getElementById("userimg").src="/asset/rockplayer.png";
     userpts=0;
     cpupts=0;
     document.getElementById("userscore").textContent=userpts;
     document.getElementById("cpuscore").textContent=cpupts;
+
+    setTimeout(remove,501);
 }
+function remove(){
+    document.getElementById("btnbox").classList.remove("fadeIn");
+    document.getElementById("start").classList.remove("fadeOut");
+    document.getElementById("score").classList.remove("fadeIn");
+}
+
 
 
 function play(val){
     if(val=="rock"){
         document.getElementById("cpuimg").src="/asset/rockcpu.png";
         document.getElementById("userimg").src="/asset/rockplayer.png";
-        setTimeout(rock,1500);
+        setTimeout(rock,1050);
+        document.getElementById("userimg").classList.add("userimg");
+        document.getElementById("cpuimg").classList.add("cpuimg");
     }
     else if(val=="paper"){
         document.getElementById("cpuimg").src="/asset/rockcpu.png";
         document.getElementById("userimg").src="/asset/rockplayer.png";
-        setTimeout(paper,1500);
+        setTimeout(paper,1050);
+        document.getElementById("userimg").classList.add("userimg");
+        document.getElementById("cpuimg").classList.add("cpuimg");
     }
     else if(val=="scissors"){
         document.getElementById("cpuimg").src="/asset/rockcpu.png";
         document.getElementById("userimg").src="/asset/rockplayer.png";
-        setTimeout(scissors,1500);
+        setTimeout(scissors,1050);
+        document.getElementById("userimg").classList.add("userimg");
+        document.getElementById("cpuimg").classList.add("cpuimg");
     }
 }
 
@@ -53,19 +82,20 @@ function rock(){
     }
     else if(cpuip=="paper"){
         document.getElementById("cpuimg").src="/asset/papercpu.png";
-        result="CPU Wins!";
+        result="CPU scores!";
         document.getElementById("out").textContent=`${result}`;
         cpupts+=1;
         updateScore(userpts,cpupts);
     }
     else if(cpuip=="scissors"){
         document.getElementById("cpuimg").src="/asset/scissorcpu.png";
-        result="User Wins!";
+        result="User scores!";
         document.getElementById("out").textContent=`${result}`;
         userpts+=1;
         updateScore(userpts,cpupts);
     }
-
+    document.getElementById("userimg").classList.remove("userimg");
+    document.getElementById("cpuimg").classList.remove("cpuimg");
 }
 
 function paper(){
@@ -75,7 +105,7 @@ function paper(){
     cpuip=cpu_arr[index];
     if(cpuip=="rock"){
         document.getElementById("cpuimg").src="/asset/rockcpu.png";
-        result="User Wins!"
+        result="User scores!"
         document.getElementById("out").textContent=`${result}`;
         userpts+=1;
         updateScore(userpts,cpupts);
@@ -91,11 +121,13 @@ function paper(){
     }
     else if(cpuip=="scissors"){
         document.getElementById("cpuimg").src="/asset/scissorcpu.png";
-        result="CPU Wins!";
+        result="CPU scores!";
         document.getElementById("out").textContent=`${result}`;
         cpupts+=1;
         updateScore(userpts,cpupts);
     }
+    document.getElementById("userimg").classList.remove("userimg");
+    document.getElementById("cpuimg").classList.remove("cpuimg");
 }
 
 function scissors(){
@@ -105,14 +137,14 @@ function scissors(){
     cpuip=cpu_arr[index];
     if(cpuip=="rock"){
         document.getElementById("cpuimg").src="/asset/rockcpu.png";
-        result="CPU Wins!";
+        result="CPU scores!";
         document.getElementById("out").textContent=`${result}`;
         cpupts+=1;
         updateScore(userpts,cpupts);
     }
     else if(cpuip=="paper"){
         document.getElementById("cpuimg").src="/asset/papercpu.png";
-        result="User Wins!"
+        result="User scores!"
         document.getElementById("out").textContent=`${result}`;
         userpts+=1;
         updateScore(userpts,cpupts);
@@ -126,7 +158,8 @@ function scissors(){
         userpts+=0;
         updateScore(userpts,cpupts);
     }
-
+    document.getElementById("userimg").classList.remove("userimg");
+    document.getElementById("cpuimg").classList.remove("cpuimg");
 }
 
 function updateScore(user,cpu){
@@ -143,11 +176,36 @@ function updateScore(user,cpu){
 }
 
 function end(player){
+    document.getElementById("retry").classList.add("fadeIn");
     document.getElementById("retry").style.visibility="visible";
-    document.getElementById("btnbox").style.visibility="hidden";
-    document.getElementById("result").textContent=`${player} wins the game!`;
+
+    document.getElementById("btnbox").classList.add("fadeOut");
+    setTimeout(function(){
+        document.getElementById("btnbox").style.visibility="hidden";
+    },501);
+    
+    document.getElementById("out").textContent=`${player} wins the game!`;
+
+    setTimeout(function(){
+        document.getElementById("retry").classList.remove("fadeIn");
+        document.getElementById("btnbox").classList.remove("fadeOut");
+    },501)
 }
 
 function retry(){
-    begin();
+    document.getElementById("retry").classList.add("fadeOut");
+    setTimeout(function(){
+        document.getElementById("retry").style.visibility="hidden";
+    },501);
+    document.getElementById("score").classList.add("fadeOut");
+    setTimeout(function(){
+        document.getElementById("score").style.visibility="hidden";
+    },501);
+    setTimeout(removeretry,501);
+    setTimeout(begin,502);
+}
+
+function removeretry(){
+    document.getElementById("retry").classList.remove("fadeOut");
+    document.getElementById("score").classList.remove("fadeOut");
 }
